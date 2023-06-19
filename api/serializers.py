eab9db1b47014fc77d78ce8eceb94b8ac4b66cac
Victor Igbokwe <igbokwev@gmail.com>
2023-06-19 10:49:33 +0100

    Update serializers.py
    code cleanup

--- api/serializers.py
+++ api/serializers.py
@@ -7,15 +7,7 @@ class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = ['id','title','description']
-    # title = serializers.CharField(max_length=100)
-    # description = serializers.CharField(max_length=100)
-
-    # def create(self, validated_data):
-    #     return Article.objects.create(validated_data)
-
-    # def update(self, instance, validated_data):
-    #     instance.title = validated_data.get('title', instance.title)
-    #     instance.description = validated_data.get('description', instance.description)
+    
 
 
 class UserSerializer(serializers.ModelSerializer):
